6db3d17e0c78f8ac4054d32f34de61b7a8fae3a9
Steve Curtis <steve.curtis@truthmd.com>
2026-07-29 14:16:38 -0400

    add cors config and postgres config

---
+++ fin-server/src/main/java/com/scurtis/finance/config/CorsGlobalConfig.java
@@ -0,0 +1,15 @@
+package com.scurtis.finance.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsGlobalConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**").allowedMethods("*");
+    }
+
+}
